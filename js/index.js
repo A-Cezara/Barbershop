25b05b1a552a4f680f83dc8fc2a1b8124839e621
A-Cezara <149768972+A-Cezara@users.noreply.github.com>
2024-03-01 00:04:56 +0200

    final

--- js/index.js
+++ js/index.js
@@ -23,3 +23,36 @@ const onBack = () => {
     });
   }
 };
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+
+  function openModal() {
+    refs.modal.style.visibility = "visible";
+  }
+  function closeModal() {
+    refs.modal.style.visibility = "hidden";
+  }
+})();
